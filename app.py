2d16444f8ea6f2df0f43cfa02b2229003eb974ef
Yanbo Liao <yliao@wipro.woolworths.com.au>
2022-01-23 20:45:25 +1100

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+import smtplib
+import time
+
+
+def gen_report():
+    return "file.cls"
+    pass
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/report', methods=['GET'])
+def report():
+    if request.method == 'GET':
+        res = gen_report()
+        return "<p>report generated</p><br><p>{res}</p>".format(res=res)
---
+++ test.Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.7
+
+LABEL maintainer=eray.liao@gmail.com
+
+WORKDIR /app
+
+COPY . .
+
+RUN python -m pip install --upgrade pip
+
+RUN pip install -r requirements.txt
+
+ENV APP_ENVIRONMENT development
+
+ENV FLASK_APP app.py
+
+EXPOSE 5000
+
+CMD ["sh", "-c", "flask run"]
